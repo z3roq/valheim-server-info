1584c2e31b6c459e58294242290292baf128e65d
z3roq <78497789+z3roq@users.noreply.github.com>
2021-03-13 18:18:15 +0200

    Create php_exapmle.php

---
+++ php_exapmle.php
@@ -0,0 +1,14 @@
+<?php
+$file = file_get_contents('game_info_file');
+$data = explode('|', $file);
+
+if ($data[0] == 1) 
+$status = "Online";
+else
+$status = "Offline";
+
+echo "Server status: ".$status."<br>";
+
+echo "Players: ".$data[1]."/10";
+
+?>
